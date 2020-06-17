f7a891d2bb3893ec844fc4ed68c93e2598499dd5
mesmacosta <mesmacosta@gmail.com>
2020-06-17 15:37:51 -0300

    ADD teradata changes

--- google-datacatalog-teradata-connector/setup.py
+++ google-datacatalog-teradata-connector/setup.py
@@ -16,9 +16,14 @@
 
 import setuptools
 
+release_status='Development Status :: 4 - Beta'
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
 setuptools.setup(
-    name='teradata2datacatalog',
-    version='1.0.0',
+    name='google-datacatalog-teradata-connector',
+    version='0.5.0',
     author='Google LLC',
     description=
     'Library for ingesting Teradata metadata into Google Cloud Data Catalog',
@@ -28,15 +33,17 @@ setuptools.setup(
     package_dir={'': 'src'},
     entry_points={
         'console_scripts': [
-            'teradata2datacatalog = google.datacatalog_connectors.teradata:main',
+            'google-datacatalog-teradata-connector = google.datacatalog_connectors.teradata:main',
         ],
     },
     include_package_data=True,
-    install_requires=('teradatasql',),
+    install_requires=('teradatasql', 'google-datacatalog-rdbms-connector'),
     setup_requires=('pytest-runner',),
-    tests_require=('pytest-cov',),
+    tests_require=('pytest-cov', 'google-datacatalog-connectors-commons-test'),
     classifiers=(
-        'Development Status :: 1 - Alpha',
+        release_status,
         'Programming Language :: Python :: 3.7',
     ),
+    long_description=readme,
+    long_description_content_type='text/markdown',
 )
