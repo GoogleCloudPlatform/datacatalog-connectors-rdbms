0c8d79ad7c248adbc1f6bbfd60615d62d98f35f1
bonifacyj <katyam_iskysoved@list.ru>
2020-09-03 08:14:55 +0000

    Scrape optional metadata from MySQL

--- google-datacatalog-mysql-connector/src/google/datacatalog_connectors/mysql_/scrape/query_assembler.py
+++ google-datacatalog-mysql-connector/src/google/datacatalog_connectors/mysql_/scrape/query_assembler.py
@@ -1,3 +1,4 @@
+import os
 from google.datacatalog_connectors.rdbms.scrape import query_assembler
 
 
@@ -5,3 +6,17 @@ class QueryAssembler(query_assembler.QueryAssembler):
 
     def _get_update_statement(self, table_name):
         return "ANALYZE TABLE {};".format(table_name)
+
+    def get_optional_queries(self, optional_metadata):
+        queries = list()
+        if 'num_rows' in optional_metadata:
+            queries.append(self._get_num_rows_query())
+        return queries
+
+    def _get_num_rows_query(self):
+        path = os.path.join(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+            'config', 'num_rows_query.sql')
+        with open(path, 'r') as f:
+            query = f.read()
+            return query
