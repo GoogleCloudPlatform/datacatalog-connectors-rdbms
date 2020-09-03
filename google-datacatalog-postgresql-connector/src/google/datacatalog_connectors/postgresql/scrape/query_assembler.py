e84a35e4f66abff3d3036f9eefdd25cd0b97adcf
bonifacyj <katyam_iskysoved@list.ru>
2020-09-03 08:23:52 +0000

    Refactor to get rid of code duplication in query assemblers

--- google-datacatalog-postgresql-connector/src/google/datacatalog_connectors/postgresql/scrape/query_assembler.py
+++ google-datacatalog-postgresql-connector/src/google/datacatalog_connectors/postgresql/scrape/query_assembler.py
@@ -7,16 +7,7 @@ class QueryAssembler(query_assembler.QueryAssembler):
     def _get_update_statement(self, table_name):
         return "ANALYZE {};".format(table_name)
 
-    def get_optional_queries(self, optional_metadata):
-        queries = list()
-        if 'num_rows' in optional_metadata:
-            queries.append(self._get_num_rows_query())
-        return queries
-
-    def _get_num_rows_query(self):
-        path = os.path.join(
+    def _get_path_to_num_rows_query(self):
+        return os.path.join(
             os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
             'config', 'num_rows_query.sql')
-        with open(path, 'r') as f:
-            query = f.read()
-            return query
--- google-datacatalog-rdbms-connector/src/google/datacatalog_connectors/rdbms/scrape/query_assembler.py
+++ google-datacatalog-rdbms-connector/src/google/datacatalog_connectors/rdbms/scrape/query_assembler.py
@@ -18,5 +18,17 @@ class QueryAssembler:
             "Implement this method to get a DB-specific update method")
 
     def get_optional_queries(self, optional_metadata):
+        queries = list()
+        if 'num_rows' in optional_metadata:
+            queries.append(self._get_num_rows_query())
+        return queries
+
+    def _get_num_rows_query(self):
+        path = self._get_path_to_num_rows_query()
+        with open(path, 'r') as f:
+            query = f.read()
+            return query
+
+    def _get_path_to_num_rows_query(self):
         raise NotImplementedError(
-            "Implement this method to get a DB-specific optional queries")
+            "Implement to deliver a DB-specific path to the num_rows query")
