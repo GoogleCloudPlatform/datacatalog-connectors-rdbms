7cdd69d2be89a26021c24ed3d3347ee345363c4f
Marcelo <mesmacosta@gmail.com>
2020-09-10 15:30:16 -0300

    FIX redshift does not support pg_enum table

---
+++ google-datacatalog-redshift-connector/src/google/datacatalog_connectors/redshift/config/metadata_query.sql
@@ -0,0 +1,31 @@
+/*
+* Copyright 2020 Google LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+SELECT  t.table_schema as schema_name,
+        t.table_name, t.table_type,
+        c.column_name,
+        c.column_default as column_default_value,
+        c.is_nullable as column_nullable,
+        c.data_type as column_type,
+        c.character_maximum_length as column_char_length,
+        c.numeric_precision as column_numeric_precision
+    FROM information_schema.tables t
+        JOIN  information_schema.columns c
+        on c.table_name = t.table_name and c.table_schema = t.table_schema
+    WHERE t.table_schema NOT IN
+        ('pg_catalog', 'information_schema',
+            'pg_toast', 'gp_toolkit', 'pg_internal')
+    ORDER BY t.table_name, c.column_name;
