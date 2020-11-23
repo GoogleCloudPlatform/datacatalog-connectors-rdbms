47831c603b48c029172898cc19da40ee945a704c
Marcelo <mesmacosta@gmail.com>
2020-11-23 11:10:31 -0300

    REMOVE breaking header from config files

--- google-datacatalog-saphana-connector/src/google/datacatalog_connectors/saphana/config/enrich_table_metadata_query.sql
+++ google-datacatalog-saphana-connector/src/google/datacatalog_connectors/saphana/config/enrich_table_metadata_query.sql
@@ -1,19 +1,3 @@
-/*
-* Copyright 2020 Google LLC
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 SELECT t.SCHEMA_NAME AS SCHEMA_NAME,
        t.TABLE_NAME AS TABLE_NAME,
        t.RECORD_COUNT AS table_rows,
