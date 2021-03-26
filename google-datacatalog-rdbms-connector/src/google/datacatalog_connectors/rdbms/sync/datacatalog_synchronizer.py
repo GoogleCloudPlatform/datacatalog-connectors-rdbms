226f29f8d5568da6f588ffd09ba85a4daf73bc10
Marcelo Costa <mesmacosta@gmail.com>
2021-03-26 17:42:24 -0300

    Fix/module loading error (#68)
    * Fix sql_objects module loading error

--- google-datacatalog-rdbms-connector/src/google/datacatalog_connectors/rdbms/sync/datacatalog_synchronizer.py
+++ google-datacatalog-rdbms-connector/src/google/datacatalog_connectors/rdbms/sync/datacatalog_synchronizer.py
@@ -26,6 +26,7 @@ from google.datacatalog_connectors.commons.monitoring\
 
 from google.datacatalog_connectors.rdbms import prepare
 from google.datacatalog_connectors.rdbms.common import constants
+from google.datacatalog_connectors.rdbms.prepare import sql_objects
 
 
 class DataCatalogSynchronizer:
@@ -121,7 +122,7 @@ class DataCatalogSynchronizer:
         prepared_entries = {constants.BASE_ENTRIES_KEY: base_entries}
 
         if self.__is_sql_objects_sync():
-            sql_objects_entry_factory = prepare.sql_objects.\
+            sql_objects_entry_factory = sql_objects.\
                 SQLObjectsAssembledEntryFactory(
                     self.__project_id,
                     self.__location_id,
@@ -234,7 +235,7 @@ class DataCatalogSynchronizer:
 
         if self.__is_sql_objects_sync():
             sql_objects_tag_template_factory = \
-                prepare.sql_objects.SQLObjectsDataCatalogTagTemplateFactory(
+                sql_objects.SQLObjectsDataCatalogTagTemplateFactory(
                     self.__project_id,
                     self.__location_id,
                     self.__entry_group_id,
