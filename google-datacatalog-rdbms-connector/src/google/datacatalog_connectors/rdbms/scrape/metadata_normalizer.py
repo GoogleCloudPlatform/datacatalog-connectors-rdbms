283ba271879046b9ea09b66932137c536026126d
HerveMignot <herve.mignot@gmail.com>
2021-04-13 10:33:42 +0200

    Reformat code

--- google-datacatalog-rdbms-connector/src/google/datacatalog_connectors/rdbms/scrape/metadata_normalizer.py
+++ google-datacatalog-rdbms-connector/src/google/datacatalog_connectors/rdbms/scrape/metadata_normalizer.py
@@ -198,7 +198,8 @@ class MetadataNormalizer:
         normalized_dict = {'name': name}
 
         if column_def['desc'] in column_metadata.columns:
-            value = cls._extract_value_from_first_row(column_metadata, column_def['desc']) 
+            value = cls._extract_value_from_first_row(column_metadata,
+                                                      column_def['desc']) 
             if not pd.isna(value):
                 normalized_dict['desc'] = value
 
